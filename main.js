8fe0c85123801943d1e5d804d13517cbbbc49b4f
Vivek Reddy <rpadigel@amazon.com>
2021-04-19 00:56:41 +0530

    indentation fix

--- main.js
+++ main.js
@@ -4,10 +4,9 @@ const path = require('path');
 const isDev = !app.isPackaged;
 
 if (isDev) {
-require('electron-reload')(__dirname, {
-	electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
-});
-
+	require('electron-reload')(__dirname, {
+		electron: path.join(__dirname, 'node_modules', '.bin', 'electron'),
+	});
 }
 
 function createWindow() {
